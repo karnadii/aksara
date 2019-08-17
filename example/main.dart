531cee57cd231e3007e4f6d523e56c0266635e3d
Ujang Karnadi <uk9313285@gmail.com>
2019-08-17 23:51:43 +0700

    format code

--- example/main.dart
+++ example/main.dart
@@ -3,5 +3,6 @@ import 'package:aksara/aksara.dart';
 void main() {
   var aksara = AksaraJava();
   print(aksara.latinToJava("aksara jawa", isMurdha: true)); //ꦄꦏ꧀ꦱꦫꦗꦮ
-  print(aksara.javaToLatin("ꦲꦏ꧀ꦱꦫꦗꦮ")); // aksarajawa (javanese script don't use space)
+  print(aksara
+      .javaToLatin("ꦲꦏ꧀ꦱꦫꦗꦮ")); // aksarajawa (javanese script don't use space)
 }
