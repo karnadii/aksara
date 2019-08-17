d4fd0b3c2e63b4ab2d45b01b27c915e1a4dbd137
Ujang Karnadi <uk9313285@gmail.com>
2019-08-17 23:46:51 +0700

    remove flutter dependencies

---
+++ example/main.dart
@@ -0,0 +1,7 @@
+import 'package:aksara/aksara.dart';
+
+void main() {
+  var aksara = AksaraJava();
+  print(aksara.latinToJava("aksara jawa", isMurdha: true)); //ꦄꦏ꧀ꦱꦫꦗꦮ
+  print(aksara.javaToLatin("ꦲꦏ꧀ꦱꦫꦗꦮ")); // aksarajawa (javanese script don't use space)
+}
--- test/aksara_test.dart
+++ test/aksara_test.dart
@@ -1,11 +1,11 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import 'package:aksara/aksara.dart';
 
 void main() {
   test('run translatiration form java to latin and vice versa', () {
     final aksara = AksaraJava();
-    expect(aksara.latinToJava("aksara jawa"), "ꦲꦏ꧀ꦱꦫ​ꦗꦮ");
+    expect(aksara.latinToJava("aksarajawa"), "ꦲꦏ꧀ꦱꦫ​ꦗꦮ");
     expect(aksara.javaToLatin("ꦲꦏ꧀ꦱꦫ​ꦗꦮ"), "aksarajawa");
     expect(() => aksara.latinToJava(null), throwsNoSuchMethodError);
     expect(() => aksara.javaToLatin(null), throwsNoSuchMethodError);
